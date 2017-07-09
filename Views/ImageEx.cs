9876225f96bd63ed9feb2194e43d97d8ebaf22c3
higeze <higeze@yahoo.co.jp>
2017-07-10 06:01:29 +0900

    20170710

---
+++ Views/ImageEx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mfg.View
+{
+    public class ImageEx : System.Windows.Controls.Image
+    {
+        //static ImageEx()
+        //{
+        //    var metaData  = SourceProperty.GetMetadata(typeof(ImageEx));
+        //    SourceProperty.OverrideMetadata(typeof(ImageEx), new FrameworkPropertyMetadata()
+        //    {
+        //        DefaultValue = metaData.DefaultValue,
+        //        CoerceValueCallback = metaData.CoerceValueCallback,
+        //        PropertyChangedCallback = new PropertyChangedCallback(prop),
+        //    });
+        //}
+
+        public ImageEx()
+            : base()
+        {
+        }
+
+        public bool IsAsync
+        {
+            get { return (bool)GetValue(IsAsyncProperty); }
+            set { SetValue(IsAsyncProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsAsync.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsAsyncProperty =
+            DependencyProperty.Register("IsAsync", typeof(bool), typeof(ImageEx),
+            new PropertyMetadata()
+            {
+                DefaultValue = true,
+                PropertyChangedCallback = new PropertyChangedCallback(prop),
+                //CoerceValueCallback = new CoerceValueCallback(coerce),
+            });
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            //var img = obj as ImageEx;
+            var bindExpress = this.GetBindingExpression(ImageEx.SourceProperty);
+            var bind = bindExpress.ParentBinding;
+            if (bind != null)
+            {
+                var newBind = new Binding()
+                {
+                    Path = bind.Path,
+                    Mode = bind.Mode,
+                    UpdateSourceTrigger = bind.UpdateSourceTrigger,
+                    IsAsync = this.IsAsync,
+
+                };
+
+                //var metaData = SourceProperty.GetMetadata(typeof(ImageEx));
+                //SourceProperty.OverrideMetadata((typeof(ImageEx), new FrameworkPropertyMetadata()
+                //{
+                //    DefaultValue = metaData.DefaultValue,
+                //    CoerceValueCallback = metaData.CoerceValueCallback,
+                //    //PropertyChangedCallback = new PropertyChangedCallback(prop),
+                //});
+
+                this.SetBinding(ImageEx.SourceProperty, newBind);
+
+                //metaData = SourceProperty.GetMetadata(typeof(ImageEx));
+                //SourceProperty.OverrideMetadata(typeof(ImageEx), new FrameworkPropertyMetadata()
+                //{
+                //    DefaultValue = metaData.DefaultValue,
+                //    CoerceValueCallback = metaData.CoerceValueCallback,
+                //    PropertyChangedCallback = new PropertyChangedCallback(prop),
+                //});
+            }
+        }
+
+        private static void prop(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var img = obj as ImageEx;
+            var bindExpress = img.GetBindingExpression(ImageEx.SourceProperty);
+            var bind = bindExpress.ParentBinding;
+            if (bind != null)
+            {
+                var newBind = new Binding()
+                {
+                    Path = bind.Path,
+                    Mode = bind.Mode,
+                    UpdateSourceTrigger = bind.UpdateSourceTrigger,
+                    IsAsync = img.IsAsync,
+
+                };
+
+                //var metaData = SourceProperty.GetMetadata(typeof(ImageEx));
+                //SourceProperty.OverrideMetadata((typeof(ImageEx), new FrameworkPropertyMetadata()
+                //{
+                //    DefaultValue = metaData.DefaultValue,
+                //    CoerceValueCallback = metaData.CoerceValueCallback,
+                //    //PropertyChangedCallback = new PropertyChangedCallback(prop),
+                //});
+
+                img.SetBinding(ImageEx.SourceProperty, newBind);
+
+                //metaData = SourceProperty.GetMetadata(typeof(ImageEx));
+                //SourceProperty.OverrideMetadata(typeof(ImageEx), new FrameworkPropertyMetadata()
+                //{
+                //    DefaultValue = metaData.DefaultValue,
+                //    CoerceValueCallback = metaData.CoerceValueCallback,
+                //    PropertyChangedCallback = new PropertyChangedCallback(prop),
+                //});
+            }
+        }
+
+        //private static object coerce(DependencyObject obj, object e)
+        //{
+        //    var img = obj as ImageEx;
+        //    var bindExpress = img.GetBindingExpression(ImageEx.SourceProperty);
+        //    var bind = bindExpress.ParentBinding;
+        //    if (bind != null)
+        //    {
+        //        var newBind = new Binding()
+        //        {
+        //            Path = bind.Path,
+        //            Mode = bind.Mode,
+        //            UpdateSourceTrigger = bind.UpdateSourceTrigger,
+        //            IsAsync = img.IsAsync,
+
+        //        };
+        //        img.SetBinding(ImageEx.SourceProperty, newBind);
+        //    }
+        //    return img.IsAsync;
+        //}
+    }
+}
---
+++ Views/ManufacturingDocument.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mfg
+{
+    /// <summary>
+    /// Interaction logic for UriList.xaml
+    /// </summary>
+    public partial class ManufacturingDocument : UserControl
+    {
+        public ManufacturingDocument()
+        {
+            InitializeComponent();
+        }
+        //private bool _isRectDragInProg;
+        //private void BlockArrow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    _isRectDragInProg = true;
+        //    (sender as UIElement).CaptureMouse();
+        //}
+
+        //private void BlockArrow_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    _isRectDragInProg = false;
+        //    (sender as UIElement).ReleaseMouseCapture();
+        //}
+
+        //private void BlockArrow_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (!_isRectDragInProg) return;
+
+        //    // get the position of the mouse relative to the Canvas
+        //    var mousePos = e.GetPosition(canvas);
+
+        //    // center the rect on the mouse
+        //    double left = mousePos.X - ((sender as Microsoft.Expression.Shapes.BlockArrow).ActualWidth / 2);
+        //    double top = mousePos.Y - ((sender as Microsoft.Expression.Shapes.BlockArrow).ActualHeight / 2);
+        //    Canvas.SetLeft((sender as Microsoft.Expression.Shapes.BlockArrow), left);
+        //    Canvas.SetTop((sender as Microsoft.Expression.Shapes.BlockArrow), top);
+        //}
+
+    }
+}
